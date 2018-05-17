1e33f3b49c4a83b39fd557737a3e92ae04eccc6b
Pankaj Saboo <31950109+pankajsaboo@users.noreply.github.com>
2018-05-17 17:55:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js");
+var routes = require("./routes/user.js");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+routes(app);
+
+var server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
+});
